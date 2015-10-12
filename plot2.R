2427d504b71ab7d8ce155d4d07b99af1e8cccfef
ayoademob <ayoademob@yahoo.co.uk>
2015-10-12 08:17:39 +0100

    CourseProject1

--- plot2.R
+++ plot2.R
@@ -4,15 +4,21 @@
 plot2 <- function(rawDT){
   # Load libraries
   library(ggplot2)
+  library(scales) # to access breaks/formatting functions
   
   # construct the plot
   rawDT = data.frame(Date = strptime(paste(rawDT[,Date],rawDT[,Time]), format="%Y-%m-%d %H:%M:%S"), 
-                     Data=rawDT[,Global_active_power])
+                    Data=rawDT[,Global_active_power])
+#   rawDT = data.frame(Date = rawDT[,Date], 
+#                      Data=rawDT[,Global_active_power])
+#   
   tmp = ggplot(rawDT, 
-         aes(Date, Data)) + 
+                aes(Date, Data)) + 
         geom_line() +
         #geom_point(data = data.frame())+
-        #scale_x_date(format = "%b") + 
+        scale_x_datetime(breaks = date_breaks("1 day")
+                         ,labels = date_format("%a") 
+                        )+ 
         xlab("") + 
         ylab("Global Active Power")
   
